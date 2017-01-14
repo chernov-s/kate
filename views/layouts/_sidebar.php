6a09a5c800da885e9e5bcc9a107b1fb799fe4b22
Sosed <chernov.net@ya.ru>
2017-01-14 12:37:08 +1000

    Add form search to main page

--- views/layouts/_sidebar.php
+++ views/layouts/_sidebar.php
@@ -4,14 +4,17 @@ use app\models\Category;
 <div class="sidebar">
     <div class="well">
         <h4>Поиск</h4>
-        <div class="input-group">
-            <input class="form-control" type="text">
-            <span class="input-group-btn">
-                <button class="btn btn-default" type="button">
-                    <span class="glyphicon glyphicon-search"></span>
-                </button>
-            </span>
-        </div>
+        <form method="get" action="index.php">
+            <div class="input-group">
+                <input type="hidden" name="r" value="category/search">
+                <input name="q" class="form-control" type="text" placeholder="Поиск по всем разделам" value="<?=isset($q) ? $q : ''?>">
+                <span class="input-group-btn">
+                    <button class="btn btn-primary" type="submit">
+                        <span class="glyphicon glyphicon-search"></span>
+                    </button>
+                </span>
+            </div>
+        </form>
     </div>
     <div class="well">
         <h4>Объявления по разделам</h4>
